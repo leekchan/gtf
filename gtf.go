05881bad8790d1cee6a23f0613df5af77e0a5578
Kyoung-chan Lee <leekchan@gmail.com>
2015-07-16 12:02:37 +0900

    GoFmt.

--- gtf.go
+++ gtf.go
@@ -1,11 +1,11 @@
 package gtf
 
 import (
-	"html/template"
-    "strings"
-	"net/url"
 	"fmt"
+	"html/template"
+	"net/url"
 	"reflect"
+	"strings"
 )
 
 // recovery will silently swallow all unexpected panics.
@@ -13,15 +13,15 @@ func recovery() {
 	recover()
 }
 
-var GtfFuncMap = template.FuncMap {
+var GtfFuncMap = template.FuncMap{
 	"stringReplace": func(s1 string, s2 string) string {
 		defer recovery()
-		
+
 		return strings.Replace(s2, s1, "", -1)
 	},
 	"stringDefault": func(s1 string, s2 string) string {
 		defer recovery()
-		
+
 		if len(s2) > 0 {
 			return s2
 		}
@@ -29,179 +29,179 @@ var GtfFuncMap = template.FuncMap {
 	},
 	"stringLength": func(s string) int {
 		defer recovery()
-		
+
 		return len(s)
 	},
 	"stringLower": func(s string) string {
 		defer recovery()
-		
+
 		return strings.ToLower(s)
 	},
 	"stringUpper": func(s string) string {
 		defer recovery()
-		
+
 		return strings.ToUpper(s)
 	},
 	"stringTruncatechars": func(n int, s string) string {
 		defer recovery()
-		
+
 		if n < 0 {
 			return s
 		}
-		
+
 		r := []rune(s)
 		rLength := len(r)
-		
+
 		if n >= rLength {
 			return s
 		}
-		
+
 		if n > 3 && rLength > 3 {
 			return string(r[:n-3]) + "..."
 		}
-		
+
 		return string(r[:n])
 	},
 	"stringUrlencode": func(s string) string {
 		defer recovery()
-		
+
 		return url.QueryEscape(s)
 	},
 	"stringWordcount": func(s string) int {
 		defer recovery()
-		
+
 		return len(strings.Fields(s))
 	},
 	"intDivisibleby": func(i1 int, i2 int) bool {
 		defer recovery()
-		
-		return i2 % i1 == 0
+
+		return i2%i1 == 0
 	},
 	"stringLengthIs": func(i int, s string) bool {
 		defer recovery()
-		
+
 		return i == len([]rune(s))
 	},
 	"stringTrim": func(s string) string {
 		defer recovery()
-		
+
 		return strings.TrimSpace(s)
 	},
 	"stringCapfirst": func(s string) string {
 		defer recovery()
-		
+
 		return strings.ToUpper(string(s[0])) + s[1:]
 	},
 	"intPluralize": func(arg string, value int) string {
 		defer recovery()
-		
+
 		if !strings.Contains(arg, ",") {
 			arg = "," + arg
 		}
-		
+
 		bits := strings.Split(arg, ",")
-		
+
 		if len(bits) > 2 {
 			return ""
 		}
-		
+
 		if value == 1 {
 			return bits[0]
 		}
-		
+
 		return bits[1]
 	},
 	"boolYesno": func(yes string, no string, value bool) string {
 		defer recovery()
-		
+
 		if value {
 			return yes
 		}
-		
+
 		return no
 	},
 	"stringRjust": func(arg int, value string) string {
 		defer recovery()
-		
+
 		n := arg - len([]rune(value))
-		
+
 		if n > 0 {
 			value = strings.Repeat(" ", n) + value
 		}
-		
+
 		return value
 	},
 	"stringLjust": func(arg int, value string) string {
 		defer recovery()
-		
+
 		n := arg - len([]rune(value))
-		
+
 		if n > 0 {
 			value = value + strings.Repeat(" ", n)
 		}
-		
+
 		return value
 	},
 	"stringCenter": func(arg int, value string) string {
 		defer recovery()
-		
+
 		n := arg - len([]rune(value))
-		
+
 		if n > 0 {
 			left := n / 2
 			right := n - left
 			value = strings.Repeat(" ", left) + value + strings.Repeat(" ", right)
 		}
-		
+
 		return value
 	},
 	"filesizeformat": func(value interface{}) string {
 		defer recovery()
-		
+
 		var size float64
-		
+
 		switch value.(type) {
-			case int, int8, int16, int32, int64:
-				size = float64(reflect.ValueOf(value).Int())
-			case uint, uint8, uint16, uint32, uint64:
-				size = float64(reflect.ValueOf(value).Uint())
-			case float32, float64:
-				size = reflect.ValueOf(value).Float()
-			default:
-				return ""
+		case int, int8, int16, int32, int64:
+			size = float64(reflect.ValueOf(value).Int())
+		case uint, uint8, uint16, uint32, uint64:
+			size = float64(reflect.ValueOf(value).Uint())
+		case float32, float64:
+			size = reflect.ValueOf(value).Float()
+		default:
+			return ""
 		}
-		
+
 		var KB float64 = 1 << 10
 		var MB float64 = 1 << 20
 		var GB float64 = 1 << 30
 		var TB float64 = 1 << 40
 		var PB float64 = 1 << 50
-		
-		filesizeFormat := func (filesize float64, suffix string) string {
+
+		filesizeFormat := func(filesize float64, suffix string) string {
 			return strings.Replace(fmt.Sprintf("%.1f %s", filesize, suffix), ".0", "", -1)
 		}
-		
-		var result string;
+
+		var result string
 		if size < KB {
 			result = filesizeFormat(size, "bytes")
 		} else if size < MB {
-			result = filesizeFormat(size / KB, "KB")
+			result = filesizeFormat(size/KB, "KB")
 		} else if size < GB {
-			result = filesizeFormat(size / MB, "MB")
+			result = filesizeFormat(size/MB, "MB")
 		} else if size < TB {
-			result = filesizeFormat(size / GB, "GB")
+			result = filesizeFormat(size/GB, "GB")
 		} else if size < PB {
-			result = filesizeFormat(size / TB, "TB")
+			result = filesizeFormat(size/TB, "TB")
 		} else {
-			result = filesizeFormat(size / PB, "PB")
+			result = filesizeFormat(size/PB, "PB")
 		}
-		
+
 		return result
 	},
 }
 
-// gtf.New is a wrapper function of template.New(http://golang.org/pkg/text/template/#New). 
-// It automatically adds the gtf functions to the template's function map 
+// gtf.New is a wrapper function of template.New(http://golang.org/pkg/text/template/#New).
+// It automatically adds the gtf functions to the template's function map
 // and returns template.Template(http://golang.org/pkg/text/template/#Template).
 func New(name string) *template.Template {
 	return template.New(name).Funcs(GtfFuncMap)
--- gtf_test.go
+++ gtf_test.go
@@ -35,7 +35,7 @@ func TestGtfFuncMap(t *testing.T) {
 
 	ParseTest(&buffer, "{{ \"The Go Programming Language\" | stringLower }}")
 	AssertEqual(t, &buffer, "the go programming language")
-	
+
 	ParseTest(&buffer, "{{ \"The Go Programming Language\" | stringUpper }}")
 	AssertEqual(t, &buffer, "THE GO PROGRAMMING LANGUAGE")
 
@@ -65,106 +65,106 @@ func TestGtfFuncMap(t *testing.T) {
 
 	ParseTest(&buffer, "{{ \"The Go\" | stringTruncatechars -1 }}")
 	AssertEqual(t, &buffer, "The Go")
-	
+
 	ParseTest(&buffer, "{{ \"http://www.example.org/foo?a=b&c=d\" | stringUrlencode }}")
 	AssertEqual(t, &buffer, "http%3A%2F%2Fwww.example.org%2Ffoo%3Fa%3Db%26c%3Dd")
-	
+
 	ParseTest(&buffer, "{{ \"The Go Programming Language\" | stringWordcount }}")
 	AssertEqual(t, &buffer, "4")
-	
+
 	ParseTest(&buffer, "{{ \"      The      Go       Programming      Language        \" | stringWordcount }}")
 	AssertEqual(t, &buffer, "4")
-	
+
 	ParseTest(&buffer, "{{ 21 | intDivisibleby 3 }}")
 	AssertEqual(t, &buffer, "true")
-	
+
 	ParseTest(&buffer, "{{ 21 | intDivisibleby 4 }}")
 	AssertEqual(t, &buffer, "false")
-	
+
 	ParseTest(&buffer, "{{ \"Go\" | stringLengthIs 2 }}")
 	AssertEqual(t, &buffer, "true")
-	
+
 	ParseTest(&buffer, "{{ \"안녕하세요.\" | stringLengthIs 6 }}")
 	AssertEqual(t, &buffer, "true")
-	
+
 	ParseTest(&buffer, "{{ \"안녕하세요. Go!\" | stringLengthIs 10 }}")
 	AssertEqual(t, &buffer, "true")
-	
+
 	ParseTest(&buffer, "{{ \"       The Go Programming Language     \" | stringTrim }}")
 	AssertEqual(t, &buffer, "The Go Programming Language")
-	
+
 	ParseTest(&buffer, "{{ \"the go programming language\" | stringCapfirst }}")
 	AssertEqual(t, &buffer, "The go programming language")
-	
+
 	ParseTest(&buffer, "You have 0 message{{ 0 | intPluralize \"s\" }}")
 	AssertEqual(t, &buffer, "You have 0 messages")
-	
+
 	ParseTest(&buffer, "You have 1 message{{ 1 | intPluralize \"s\" }}")
 	AssertEqual(t, &buffer, "You have 1 message")
-	
+
 	ParseTest(&buffer, "0 cand{{ 0 | intPluralize \"y,ies\" }}")
 	AssertEqual(t, &buffer, "0 candies")
-	
+
 	ParseTest(&buffer, "1 cand{{ 1 | intPluralize \"y,ies\" }}")
 	AssertEqual(t, &buffer, "1 candy")
-	
+
 	ParseTest(&buffer, "2 cand{{ 2 | intPluralize \"y,ies\" }}")
 	AssertEqual(t, &buffer, "2 candies")
-	
+
 	ParseTest(&buffer, "{{ 2 | intPluralize \"y,ies,s\" }}")
 	AssertEqual(t, &buffer, "")
-	
+
 	ParseTest(&buffer, "{{ true | boolYesno \"yes~\" \"no~\" }}")
 	AssertEqual(t, &buffer, "yes~")
-	
+
 	ParseTest(&buffer, "{{ false | boolYesno \"yes~\" \"no~\" }}")
 	AssertEqual(t, &buffer, "no~")
-	
+
 	ParseTest(&buffer, "{{ \"Go\" | stringRjust 10 }}")
 	AssertEqual(t, &buffer, "        Go")
-	
+
 	ParseTest(&buffer, "{{ \"안녕하세요\" | stringRjust 10 }}")
 	AssertEqual(t, &buffer, "     안녕하세요")
-	
+
 	ParseTest(&buffer, "{{ \"Go\" | stringLjust 10 }}")
 	AssertEqual(t, &buffer, "Go        ")
-	
+
 	ParseTest(&buffer, "{{ \"안녕하세요\" | stringLjust 10 }}")
 	AssertEqual(t, &buffer, "안녕하세요     ")
-	
+
 	ParseTest(&buffer, "{{ \"Go\" | stringCenter 10 }}")
 	AssertEqual(t, &buffer, "    Go    ")
-	
+
 	ParseTest(&buffer, "{{ \"안녕하세요\" | stringCenter 10 }}")
 	AssertEqual(t, &buffer, "  안녕하세요   ")
-	
+
 	ParseTest(&buffer, "{{ 123456789 | filesizeformat }}")
 	AssertEqual(t, &buffer, "117.7 MB")
-	
+
 	ParseTest(&buffer, "{{ 234 | filesizeformat }}")
 	AssertEqual(t, &buffer, "234 bytes")
-	
+
 	ParseTest(&buffer, "{{ 12345 | filesizeformat }}")
 	AssertEqual(t, &buffer, "12.1 KB")
-	
+
 	ParseTest(&buffer, "{{ 554832114 | filesizeformat }}")
 	AssertEqual(t, &buffer, "529.1 MB")
-	
+
 	ParseTest(&buffer, "{{ 1048576 | filesizeformat }}")
 	AssertEqual(t, &buffer, "1 MB")
-	
+
 	ParseTest(&buffer, "{{ 14868735121 | filesizeformat }}")
 	AssertEqual(t, &buffer, "13.8 GB")
-	
+
 	ParseTest(&buffer, "{{ 14868735121365 | filesizeformat }}")
 	AssertEqual(t, &buffer, "13.5 TB")
-	
+
 	ParseTest(&buffer, "{{ 1486873512136523 | filesizeformat }}")
 	AssertEqual(t, &buffer, "1.3 PB")
-	
+
 	ParseTest(&buffer, "{{ 12345.35335 | filesizeformat }}")
 	AssertEqual(t, &buffer, "12.1 KB")
-	
+
 	ParseTest(&buffer, "{{ 4294967293 | filesizeformat }}")
 	AssertEqual(t, &buffer, "4 GB")
 }
